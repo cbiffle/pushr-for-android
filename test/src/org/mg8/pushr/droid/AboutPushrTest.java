37cef0ad2f4d406e7a45d16e5741c6ea01fec4a3
Cliff L. Biffle <cbiffle+github@gmail.com>
2009-01-11 01:26:45 -0800

    Linkified about box, with test.

---
+++ test/src/org/mg8/pushr/droid/AboutPushrTest.java
@@ -0,0 +1,10 @@
+package org.mg8.pushr.droid;
+
+import android.test.ActivityInstrumentationTestCase;
+
+public class AboutPushrTest extends ActivityInstrumentationTestCase<AboutPushr> {
+  
+  public AboutPushrTest() {
+    super("org.mg8.pushr.droid", AboutPushr.class);
+  }
+}
